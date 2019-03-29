d14174a802e56f886e90de003077b06e6d8acfff
Nathaniel Cantwell <ndcantwell@gmail.com>
2019-03-29 23:50:37 +0000

    Write script to load data

---
+++ scripts/loadData.sql
@@ -0,0 +1,7 @@
+USE punocracy;
+
+/* Load data from the file of parsed homophones */
+LOAD DATA INFILE '/var/lib/mysql-files/parsedHomophones.csv'
+	INTO TABLE Words_T
+	FIELDS TERMINATED BY ','
+	LINES TERMINATED BY '\n';
