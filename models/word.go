658b63e09e5fb32e06099ad8c6e4ef6895d4a631
aronorth <aronorth@live.com>
2019-04-14 18:47:11 -0400

    changed models/word and moved module to seperate directory due to conflicting logic

--- models/word.go
+++ models/word.go
@@ -2,7 +2,7 @@ package models
 
 // Word is the core of our project
 type Word struct {
-	WordID         int    `bson:"wordID"`
-	Word           string `bson:"word"`
-	HomophoneGroup int    `bson:"homophoneGroup"`
+	WordID         int    `db:"wordID"`
+	Word           string `db:"word"`
+	HomophoneGroup int    `db:"homophoneGroup"`
 }
