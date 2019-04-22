989556d8638a9ac04edcf3b02d8ee74bc4edb86e
Nathaniel Cantwell <ndcantwell@gmail.com>
2019-04-22 01:04:21 +0000

    Create UserRating struct with pretty print

--- models/ratingHistory.go
+++ models/ratingHistory.go
@@ -11,13 +11,42 @@ import (
 	"time"
 )
 
+// A single User Rating; a log of the phrase, rating, and date it was rated
+type UserRating struct {
+	PhraseID    primitive.ObjectID `bson:"phraseID"`
+	RatingValue int                `bson:"ratingValue"`
+	RateDate    time.Time          `bson:"rateDate"`
+}
+
+// Pretty printing
+func (u UserRating) String() string {
+	formatString := `{
+	phraseID: ObjectID("%v")
+	rateDate: %v
+}`
+	return fmt.Sprintf(formatString, u.PhraseID, u.RatingValue, u.RateDate)
+}
+
 // Adds a rating for a specific user
-func AddRating(user UserRow, ratedPhrase Phrase, ratingHistory *mongo.Collection) error {
+func AddRating(user UserRow, rating int, ratedPhrase Phrase, ratingHistory *mongo.Collection) error {
 	//
+	return nil
 }
 
 // TODO: write ChangeRating function
+func ChangeRating(user UserRow, rating int, ratedPhrase Phrase, ratingHistory *mongo.Collection) error {
+	return nil
+}
+
 // TODO: write DeleteRating function
+func DeleteRating(user UserRow, rating int, ratedPhrase Phrase, ratingHistory *mongo.Collection) error {
+	return nil
+}
+
 // TODO: write GetRatingsByUserID function (sorted by date)
+func GetRatingsByUserID(user UserRow, ratingHistory *mongo.Collection) ([]UserRating, error) {
+	return nil, nil
+}
+
 // TODO: write DeleteUserRatings function
 // NOTE: Make everything propagate to the phrases table
