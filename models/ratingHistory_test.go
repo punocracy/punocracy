82c32ba50d3ce4e836f3e2247e4b8d4d48218356
Nathaniel Cantwell <ndcantwell@gmail.com>
2019-04-22 03:35:07 +0000

    Write AddRating function

--- models/ratingHistory_test.go
+++ models/ratingHistory_test.go
@@ -1,6 +1,11 @@
 package models
 
 // TODO: write TestAddRating function
+// TestAddRating tests the AddRating function
+func TestAddRating(t *testing.T) {
+
+}
+
 // TODO: write TestChangeRating function
 // TODO: write TestDeleteRating function
 // TODO: write TestGetRatingsByUserID function (sorted by date)
