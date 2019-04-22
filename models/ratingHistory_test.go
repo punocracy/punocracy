9c6248d22562579e018bf37e0eaa732f53727b26
Nathaniel Cantwell <ndcantwell@gmail.com>
2019-04-22 00:44:03 +0000

    Add test file for ratingHistory

---
+++ models/ratingHistory_test.go
@@ -0,0 +1,7 @@
+package models
+
+// TODO: write TestAddRating function
+// TODO: write TestChangeRating function
+// TODO: write TestDeleteRating function
+// TODO: write TestGetRatingsByUserID function (sorted by date)
+// TODO: write TestDeleteUserRatings function
