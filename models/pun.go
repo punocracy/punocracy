c3ba3251c92157b722366e389e1714cbad53c30a
Alvaro Esperanca <alvaro.sdb.esperanca@gmail.com>
2019-04-22 13:59:25 -0400

    Maybe it be work

--- models/pun.go
+++ models/pun.go
@@ -2,34 +2,26 @@ package models
 
 import (
 	"strings"
-
-	"github.com/Sirupsen/logrus"
 )
 
 // GeneratePuns given query word, homophone word list, and phrase
 func GeneratePuns(word string, homophoneWords []WordRow, phrases []Phrase) []string {
 	puns := []string{}
 
+	// TODO: remove punctuation
 	for _, phrase := range phrases {
 		tokens := strings.Split(phrase.PhraseText, " ")
-		logrus.Infoln("Tokens", tokens)
-		text := []string{}
 
-		for _, token := range tokens {
+		for i, token := range tokens {
 			for _, homophoneWord := range homophoneWords {
 				if strings.ToLower(token) == homophoneWord.Word {
-					text = append(text, word)
-					logrus.Infoln("Text Match", text)
-				} else {
-					text = append(text, token)
-					logrus.Infoln("Text No Match", text)
+					tokens[i] = word
+					break
 				}
 			}
 		}
-		result := strings.Join(text, " ")
-		logrus.Infoln("Result", result)
+		result := strings.Join(tokens, " ")
 		puns = append(puns, result)
-		logrus.Infoln("Puns", puns)
 	}
 
 	return puns
