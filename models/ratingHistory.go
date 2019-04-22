2ba7f81433a267e44313ef7469f78dd644c9f021
Nathaniel Cantwell <ndcantwell@gmail.com>
2019-04-22 00:33:17 +0000

    Create file ratingHistory.go

---
+++ models/ratingHistory.go
@@ -0,0 +1,14 @@
+// User rating history support functions
+
+package models
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+// TODO: everything
